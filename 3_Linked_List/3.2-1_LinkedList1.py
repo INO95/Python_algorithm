e0352fbf65139b5247a83f987102b657727ba893
INO <bih0905@naver.com>
2023-02-02 22:55:41 +0900

    230202

--- 3_Linked_List/3.2-1_LinkedList1.py
+++ 3_Linked_List/3.2-1_LinkedList1.py
@@ -7,5 +7,27 @@ class Node:
         self.next = None
 
 
-node = Node(3)
-print(f'{node.data}')
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def traverse(self):
+        temp = self.head
+
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+Linked_list = LinkedList()
+
+node1 = Node(11)
+Linked_list.head = node1
+node2 = Node(12)
+node3 = Node(13)
+
+node1.next = node2
+node2.next = node3
+
+Linked_list.traverse()
