4bcbd957399f810994c32f7e07175f5cf1aaccdd
INO <bih0905@naver.com>
2023-07-22 21:50:16 +0900

    230722

---
+++ 230718~/2307/230722/230722.py
@@ -0,0 +1,17 @@
+# 정렬된 배열과 목푯값이 주어지는데 목푯값을 찾는다면 배열의 해당 인덱스를 반환하고,
+# 찾지 못한다면 정렬된 배열이 되도록 목표값이 배열에 들어가야 하는 인덱스를 구하라
+
+nums = [1, 2, 4, 5]
+target = 3
+target2 = 4
+
+
+def findIndex(nums, target) -> int:
+    for i in range(0, len(nums)):
+        if (nums[i] == target):
+            return print("index is : ", i)
+        if (nums[i] + 1 == target):
+            return print("index is ", i + 1)
+
+
+findIndex(nums, target2)
