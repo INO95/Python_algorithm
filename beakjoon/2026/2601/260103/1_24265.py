d20e9e1aa44b653c93f01006a58b147c401b7f31
INO <bih0905@naver.com>
2026-01-03 15:29:14 +0900

    260103

---
+++ beakjoon/2026/2601/260103/1_24265.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+
+# count = 0
+
+# for i in range(1, n - 1 + 1):
+#     for j in range(i + 1, n + 1):
+#         count = count + 1
+
+# print(count)
+# print(2)
+
+print((n - 1) * n // 2)
+print(2)
