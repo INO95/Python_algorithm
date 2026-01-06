bf5a10df49bbb05fe95b679f2826de6544925f99
INO <bih0905@naver.com>
2026-01-06 20:33:26 +0900

    260106

---
+++ beakjoon/2026/2601/260106/1_24266.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+# count = 0
+
+# for i in range(1, n + 1):
+#     for j in range(1, n + 1):
+#         for k in range(1, n + 1):
+#             count = count + 1
+
+# print(count)
+# print(n * n * n)
+# gemini feedback
+print(n**3)
+print(3)
